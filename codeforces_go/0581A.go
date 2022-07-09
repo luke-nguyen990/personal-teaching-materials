b4c7acdaad100c9ed963a7040d0a8717f24f3327
Luke Nguyen <luke_nguyen@mckinsey.com>
2022-07-09 13:17:31 +0700

    codeforces: 581A

---
+++ codeforces_go/0581A.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a float64
+	var b float64
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+
+var ans1 int = int(math.Min(a, b))
+	var ans2 int = int(math.Max(a, b)-math.Min(a, b)) / 2
+
+	fmt.Printf("%d %d\n", ans1, ans2)
+}
